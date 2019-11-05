43ed885cc3a3606e1cdefd94e76d1bea8de21d62
haggistech <haggistech@gmail.com>
2019-11-05 13:36:20 +0000

    Tom Example

---
+++ main.tf
@@ -0,0 +1,73 @@
+# Own personal acct details for sandbox testing
+
+provider "aws" {
+    region     = "eu-west-1"
+}
+
+resource "aws_security_group" "mik_terra" {
+  name        = "mik_terra"
+  description = "Allow SSH in from devlan"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "TCP"
+    cidr_blocks = ["195.27.20.114/32"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "TCP"
+    cidr_blocks = ["87.155.238.50/32"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "TCP"
+    self        = true
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "TCP"
+    cidr_blocks = ["86.155.238.50/32"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "TCP"
+    cidr_blocks = ["86.155.238.50/32"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "TCP"
+    cidr_blocks = ["86.155.238.50/32"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "terraform1" {
+  ami           = "ami-bb9a6bc2"
+  instance_type = "t2.micro"
+  key_name = "mikDev"
+  security_groups = [ "mik_terra" ]
+  ebs_block_device {
+    device_name = "/dev/sdb"
+    volume_size = 5
+    volume_type = "gp2"
+    delete_on_termination = true
+  }
+}
+
--- region.tf
+++
@@ -1,5 +0,0 @@
-module "eu-west-1" {
-  source          = "./region"
-  region          = "eu-west-1"
-  base_cidr_block = "${var.base_cidr_block}"
-}
